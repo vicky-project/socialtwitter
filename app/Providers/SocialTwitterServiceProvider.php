fc86ec2f5c7e014fe5344a5e2f17e106db567b16
vicky <pratamavr@gmail.com>
2026-06-09 19:40:09 +0800

    updates

--- app/Providers/SocialTwitterServiceProvider.php
+++ app/Providers/SocialTwitterServiceProvider.php
@@ -32,6 +32,8 @@ class SocialTwitterServiceProvider extends ServiceProvider
     $this->registerConfig();
     $this->registerViews();
     $this->loadMigrationsFrom(module_path($this->name, 'database/migrations'));
+
+    $this->mergeConfigFrom(module_path($this->name, 'config/twitter.php'), 'services');
   }
 
   /**
---
+++ config/twitter.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+  'twitter' => [
+    'client_id' => env('TWITTER_CLIENT_ID'),
+    'client_secret' => env('TWITTER_CLIENT_SECRET'),
+    'redirect' => env('TWITTER_REDIRECT_URI')
+  ],
+];
